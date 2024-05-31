cfbdc0770b27f4a8678f49f7d55201efa14a4340
Matvey Eikholts <matze85eikholts@gmail.com>
2024-05-31 16:29:41 +0300

    Idea project for guskova book examples

---
+++ 01-java-code-from-books/01-oop-guskova/time-example/TestTime.java
@@ -0,0 +1,7 @@
+public class TestTime {
+
+  public static void main(String[] args) {
+    Time time = new Time(1, 2, 3);
+    System.out.println(time);
+  }
+}
--- 01-java-code-from-books/01-oop-guskova/time-example/Time.java
+++ 01-java-code-from-books/01-oop-guskova/time-example/Time.java
@@ -1,4 +1,9 @@
 public class Time {
+  private int second, minute, hour;
 
-  public Time() {}
+  public Time() {
+    this.second = 0;
+    this.minute = 0;
+    this.hour = 0;
+  }
 }
